967bd089dd3ced3e40d083c9d81d438854512c6b
CHANDARA-code <chandaraleav97@gmail.com>
2025-08-24 14:31:58 +0700

    init(flutter)

---
+++ lib/text_widget/text_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(
+    MaterialApp(
+      home: TextWidget(),
+    ),
+  );
+}
+
+class TextWidget extends StatelessWidget {
+  const TextWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Text"),
+        backgroundColor: Colors.amberAccent,
+      ),
+      body: Center(
+        child: Text("Text Widget Basic"),
+      ),
+    );
+  }
+}
